336993a87dc15bd2822b88aff606e13bcbe4e6ef
weibin <cwb@chenweibindeMacBook-Pro.local>
2014-07-08 00:28:12 +0800

    Initial Commit

---
+++ 1111/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  1111
+//
+//  Created by weibin on 14-7-8.
+//  Copyright (c) 2014年 cwb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
